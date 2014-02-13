73291215759568605a4fb42e3d0e41d9560bfc42
Marc Chevrette <chevrm@gmail.com>
2014-02-13 18:27:32 -0500

    -nt tag added for no trimming

--- src/mrmuscle.pl
+++ src/mrmuscle.pl
@@ -20,9 +20,15 @@
 use strict;
 use warnings;
 
-## usage:  perl mrmuscle.pl file.fa
+## usage:  perl mrmuscle.pl file.fa (-nt)
 
 my $fasta = shift;
+my $nt = 0;
+foreach (@ARGV){
+	if ($_ eq '-nt'){
+		$nt=1;
+	}
+}
 my $mmdir = $0;
 $mmdir =~ s/mrmuscle$//;
 $mmdir =~ s/src\/mrmuscle.pl$//;
@@ -40,12 +46,15 @@ $root =~ s/.fasta//g;
 $root =~ s/.fa+//g;
 $root =~ s/.aa//g;
 system("muscle -in $fasta -out $root.afa -maxiters 1000");
-system("perl $mmdir/src/muscletrim.pl $root.afa > $root.trim.afa");
-system("perl $mmdir/src/preseqconv.pl $root.trim.afa $root.trimfi.afa");
-system("perl $mmdir/src/seqconv.pl -d$root.trimfi.afa -if -on");
-system("perl $mmdir/src/seqconv.pl -d$root.trimfi.afa -if -opc");
-system("perl $mmdir/src/nexdna2prot.pl $root.trimfi.nex > $root.trimfi.prot.nex");
+unless ($nt == 1){
+	system("perl $mmdir/src/muscletrim.pl $root.afa > $root.trim.afa");
+	$root = "$root.trim";
+}
+system("perl $mmdir/src/preseqconv.pl $root.afa $root.fi.afa");
+system("perl $mmdir/src/seqconv.pl -d$root.fi.afa -if -on");
+system("perl $mmdir/src/seqconv.pl -d$root.fi.afa -if -opc");
+system("perl $mmdir/src/nexdna2prot.pl $root.fi.nex > $root.fi.prot.nex");
 my $quart = sprintf("%.0f", 0.25*$seqc);
-system("perl $mmdir/src/nexappmb.pl $root.trimfi.prot.nex $ngen $quart");
-system("$mmdir/src/mrbayes $root.trimfi.prot.nex");
-system("figtree $root.trimfi.prot.nex.con.tre");
+system("perl $mmdir/src/nexappmb.pl $root.fi.prot.nex $ngen $quart");
+system("$mmdir/src/mrbayes $root.fi.prot.nex");
+system("figtree $root.fi.prot.nex.con.tre");
